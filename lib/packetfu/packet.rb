97fa856b8bb7d9e6c5d8adc9969f8118fd9d6177
todb <todb@planb-security.net>
2008-09-12 18:41:11 +0000

    * Namespacing on Config.
    git-svn-id: https://packetfu.googlecode.com/svn/trunk@41 ebf01b17-4b54-0410-ae1b-bdaaa366248d

--- lib/packetfu/packet.rb
+++ lib/packetfu/packet.rb
@@ -102,14 +102,14 @@ module PacketFu
 
 		# Put the entire packet into a libpcap file.
 		def to_f(filename=nil)
-			PacketFu::Write.a2f(:file=> filename || Config.new.config[:pcapfile],
+			PacketFu::Write.a2f(:file=> filename || PacketFu::Config.new.config[:pcapfile],
 													:arr=>[@headers[0].to_s])
 		end
 
 		# Put the entire packet on the wire by creating a temporary PacketFu::Inject object.
 		# TODO: Do something with auto-checksumming?
 		def to_w(iface=nil)
-			inj = PacketFu::Inject.new(:iface => (iface || Config.new.config[:iface]))
+			inj = PacketFu::Inject.new(:iface => (iface || PacketFu::Config.new.config[:iface]))
 			inj.array = [@headers[0].to_s]
 			inj.inject
 		end
