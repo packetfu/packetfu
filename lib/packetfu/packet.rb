ac8e1d983dc8b0525f1802e3f8d393e3c7da9bf1
todb <todb@planb-security.net>
2009-02-23 23:43:12 +0000

    Quick fix for another tmanning bug.
    git-svn-id: https://packetfu.googlecode.com/svn/trunk@69 ebf01b17-4b54-0410-ae1b-bdaaa366248d

--- lib/packetfu/packet.rb
+++ lib/packetfu/packet.rb
@@ -181,13 +181,17 @@ module PacketFu
 							else
 								tcp_all = io[ip_hlen+14,0xffff] # Don't believe the tcp_len value; suck everything up.
 							end
-							tcp_hlen = ((tcp_all[12,1].unpack("C")[0]) >> 4) * 4
-							tcp_opts = tcp_all[20,tcp_hlen-20]
-							tcp_body = tcp_all[tcp_hlen,0xffff]
-							@tcp_header.read(tcp_all[0,20])
-							@tcp_header.tcp_opts=tcp_opts
-							@tcp_header.body=tcp_body
-							@ip_header.body = @tcp_header
+							tcp_hlen =  ((tcp_all[12,1].unpack("C")[0]) >> 4) * 4
+							if tcp_hlen.to_i >= 20
+								tcp_opts = tcp_all[20,tcp_hlen-20]
+								tcp_body = tcp_all[tcp_hlen,0xffff]
+								@tcp_header.read(tcp_all[0,20])
+								@tcp_header.tcp_opts=tcp_opts
+								@tcp_header.body=tcp_body
+								@ip_header.body = @tcp_header
+							else # It's a TCP packet with an impossibly small hlen, so it can't be real TCP. Abort! Abort!
+								@ip_header.body = io[16,io.size-16]
+							end
 						elsif ip_proto_num == 0x11 # It's UDP.
 							udp_len = io[16,2].unpack("n")[0] - 20
 							if args[:strip] # Same deal as with TCP. We might have stuff at the end of the packet that's not part of the payload.
