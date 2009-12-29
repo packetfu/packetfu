3624a7b31d687f3c3b5cec5814fb60a00c73f16c
todb <todb@planb-security.net>
2009-12-29 18:41:59 +0000

    Cleaning up some whitespace weirdness.
    git-svn-id: https://packetfu.googlecode.com/svn/branches/struct_version@113 ebf01b17-4b54-0410-ae1b-bdaaa366248d

--- test/ptest.rb
+++ test/ptest.rb
@@ -3,9 +3,4 @@ $: << File.expand_path(File.dirname(__FILE__) + "/../lib/")
 require 'packetfu_s'
 include PacketFu
 
-@s = PcapFile.new.f2a(:f=>'sample.pcap').first
-@u = UDPPacket.new
-@u.read @s
-
-#@u.payload = @u.payload.gsub(/metasploit/,"MeatPistol")
-
+@p = PcapFile.new
