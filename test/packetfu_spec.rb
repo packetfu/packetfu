47f406d19234137bf8d1baf58df455cbe4e33f3b
todb <todb@planb-security.net>
2011-05-30 01:24:05 +0000

    Require rspec 2.6. Not a horrible method to do that. Also increment the version number
    git-svn-id: https://packetfu.googlecode.com/svn/trunk@173 ebf01b17-4b54-0410-ae1b-bdaaa366248d

--- test/packetfu_spec.rb
+++ test/packetfu_spec.rb
@@ -1,8 +1,13 @@
 require File.join("..","lib","packetfu")
 
+unless %x{#{$0} --version} =~ /^2\.6/
+	puts "PacketFu needs rspec 2.6 or so."
+	exit 1
+end
+
 describe PacketFu, "version information" do
 	it "reports a version number" do
-		PacketFu::VERSION.should == "1.0.2"
+		PacketFu::VERSION.should == "1.0.3"
 	end
 	its(:version) {should eq PacketFu::VERSION} 
 
