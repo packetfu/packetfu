fe630105e643f1bb0495a46676f1013e74988cd0
Jonathan Claudius <jclaudius@trustwave.com>
2015-04-26 23:17:00 -0400

    Add Ubuntu 14.04 #ifconfig spec example

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -13,6 +13,7 @@ describe Utils do
     end
 
     it "should work on Mac OSX Yosemite" do
+      stub_const("RUBY_PLATFORM", "x86_64-darwin14")
       mac_osx_reply = "ifconfig en0\n" + 
                       "en0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500\n" +
                       "ether 78:31:c1:ce:39:bc\n" +
@@ -37,5 +38,33 @@ describe Utils do
       expect(util_reply[:ip_src]).to eq("\xC0\xA8\n\xAD")
       expect(util_reply[:ip4_obj]).to eq(IPAddr.new("192.168.10.0/24"))
     end
+
+    it "should work on Ubuntu 14.04 LTS" do
+      stub_const("RUBY_PLATFORM", "x86_64-linux")
+      ubuntu_reply = "eth0      Link encap:Ethernet  HWaddr 00:0c:29:2a:e3:bd\n" + 
+                     "inet addr:192.168.10.174  Bcast:192.168.10.255  Mask:255.255.255.0\n" + 
+                     "inet6 addr: fe80::20c:29ff:fe2a:e3bd/64 Scope:Link\n" + 
+                     "UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1\n" + 
+                     "RX packets:65782 errors:0 dropped:0 overruns:0 frame:0\n" + 
+                     "TX packets:31354 errors:0 dropped:0 overruns:0 carrier:0\n" + 
+                     "collisions:0 txqueuelen:1000\n" + 
+                     "RX bytes:40583515 (40.5 MB)  TX bytes:3349554 (3.3 MB)"
+      allow(PacketFu::Utils).to receive(:ifconfig_data_string).and_return(ubuntu_reply)
+      util_reply = PacketFu::Utils.ifconfig("eth0")
+
+      # Ensure we got a hash back
+      expect(util_reply).to be_a(::Hash)
+
+      # Ensure all our values parse correctly
+      expect(util_reply[:iface]).to eq("eth0")
+      expect(util_reply[:eth_saddr]).to eq("00:0c:29:2a:e3:bd")
+      expect(util_reply[:eth_src]).to eq("\x00\f)*\xE3\xBD")
+      expect(util_reply[:ip6_saddr]).to eq("fe80::20c:29ff:fe2a:e3bd/64")
+      expect(util_reply[:ip6_obj]).to eq(IPAddr.new("fe80::20c:29ff:fe2a:e3bd/64"))
+      expect(util_reply[:ip_saddr]).to eq("192.168.10.174")
+      expect(util_reply[:ip_src]).to eq("\xC0\xA8\n\xAE")
+      expect(util_reply[:ip4_obj]).to eq(IPAddr.new("192.168.10.0/24"))
+    end
+
   end
 end
