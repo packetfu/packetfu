4b06e1dc778a95fb09899a598ccef1cee1795f3f
Jonathan Claudius <jclaudius@trustwave.com>
2015-04-26 21:56:29 -0400

    Remove scope creep

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -4,10 +4,6 @@ include PacketFu
 
 describe Utils do
   context "when using ifconfig" do
-    it "should return a hash" do
-      PacketFu::Utils.ifconfig().should be_a(::Hash)
-    end
-
     it "should prevent non-interface values" do
       expect {
         PacketFu::Utils.ifconfig("not_an_interface")
