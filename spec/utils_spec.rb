dff1bf9468376db9bfd214716477cc7e40aababe
Jonathan Claudius <jclaudius@trustwave.com>
2015-04-26 21:50:25 -0400

    Add a spec to test interface validatation

---
+++ spec/utils_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+include PacketFu
+
+describe Utils do
+  context "when using ifconfig" do
+    it "should return a hash" do
+      PacketFu::Utils.ifconfig().should be_a(::Hash)
+    end
+
+    it "should prevent non-interface values" do
+      expect {
+        PacketFu::Utils.ifconfig("not_a_interface")
+      }.to raise_error(ArgumentError)
+    end
+  end
+end
