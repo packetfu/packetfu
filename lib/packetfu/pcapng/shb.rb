5ca84e6a1846dcf04e02a6c85fa3f8e76ce94e5d
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-04-24 19:00:22 +0200

    Pcapng::SHB: fix endianness when reading a file of different endianness than one defined at SHB object creation.
    In #read, call new method #force_endianness instead of #set_endianness
    and #init_fields, which sets args but not self.

--- lib/packetfu/pcapng/shb.rb
+++ lib/packetfu/pcapng/shb.rb
@@ -41,7 +41,7 @@ module PacketFu
 
       # Used by #initialize to set the initial fields
       def init_fields(args={})
-        args[:type]  = args[:type] ? @int32.new(args[:type]) : PcapNG::SHB_TYPE
+        args[:type]  = @int32.new(args[:type] || PcapNG::SHB_TYPE.to_i)
         args[:block_len] = @int32.new(args[:block_len] || MIN_SIZE)
         args[:magic] = @int32.new(args[:magic] || MAGIC_INT32)
         args[:ver_major] = @int16.new(args[:ver_major] || 1)
@@ -79,8 +79,7 @@ module PacketFu
           case magic_str
           when MAGIC_LITTLE
           when MAGIC_BIG
-            set_endianness :big
-            init_fields
+            force_endianness :big
           else
             raise InvalidFileError, 'Incorrect magic for Section Header Block'
           end
@@ -88,8 +87,7 @@ module PacketFu
           case magic_str
           when MAGIC_BIG
           when MAGIC_LITTLE
-            set_endianness :little
-            init_fields
+            force_endianness :little
           else
             raise InvalidFileError, 'Incorrect magic for Section Header Block'
           end
@@ -125,6 +123,21 @@ module PacketFu
         pad_field :options
         recalc_block_len
         to_a.map(&:to_s).join + body
+      end
+
+
+      private
+
+      def force_endianness(endian)
+        set_endianness endian
+        @endian = endian
+        self[:type]  = @int32.new(self[:type].to_i)
+        self[:block_len] = @int32.new(self[:block_len].to_i)
+        self[:magic] = @int32.new(self[:magic].to_i)
+        self[:ver_major] = @int16.new(self[:ver_major].to_i)
+        self[:ver_minor] = @int16.new(self[:ver_minor].to_i)
+        self[:section_len] = @int64.new(self[:section_len].to_i)
+        self[:block_len2] = @int32.new(self[:block_len2].to_i)
       end
 
     end
