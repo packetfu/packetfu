fbb6fc610f6f1d03e7bdb55c5f975c2a6c0ce697
Dimitris Grigoriou <dimitris.riou@gmail.com>
2016-12-07 18:00:46 +0200

    Add Neighbor Discovery ndp_calc_sum

--- lib/packetfu/protos/ndp.rb
+++ lib/packetfu/protos/ndp.rb
@@ -53,6 +53,28 @@ module PacketFu
 
     # Calculates the checksum for the object.
     def ndp_calc_sum
+      checksum = 0
+
+      # Compute sum on pseudo-header
+      [ipv6_src, ipv6_dst].each do |iaddr|
+        8.times { |i| checksum += (iaddr >> (i*16)) & 0xffff }
+      end
+      checksum += PacketFu::NDPHeader::PROTOCOL_NUMBER
+      checksum += ipv6_len
+      # Continue with entire ICMPv6 message.
+      checksum += (ndp_type.to_i << 8) + ndp_code.to_i
+      checksum += ndp_reserved.to_i >> 16
+      checksum += ndp_reserved.to_i & 0xffff
+      8.times { |i| checksum += (ndp_tgt.to_i >> (i*16)) & 0xffff }
+      checksum += (ndp_opt_type.to_i << 8) + ndp_opt_len.to_i
+
+      mac2int = ndp_lla.to_s.unpack('H*').first.to_i(16)
+      3.times { |i| checksum += (mac2int >> (i*16)) & 0xffff }
+
+      checksum = checksum % 0xffff
+      checksum = 0xffff - checksum
+      checksum == 0 ? 0xffff : checksum
+
     end
 
     # Recalculates the calculatable fields for ICMPv6.
