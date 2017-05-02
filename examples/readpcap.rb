bb51ad9bcda8e9a418ca094b9bb2314c6b3cdb13
Jonathan Claudius <jclaudius@mozilla.com>
2017-05-02 15:51:25 -0400

    Add packet parsing example

---
+++ examples/readpcap.rb
@@ -0,0 +1,23 @@
+# Usage:
+# rvmsudo ruby examples/readpcap.rb test.pcap test.pcap
+
+# Path setting slight of hand:
+$: << File.expand_path("../../lib", __FILE__)
+
+require 'packetfu'
+
+pcap_filename = ARGV[0].chomp
+
+unless File.exists?(pcap_filename)
+  puts "PCAP input file #{pcap_filename} could not be found"
+end
+
+puts "Reading PCAP to packet array from #{File.expand_path(pcap_filename)}"
+packet_array = PacketFu::PcapFile.file_to_array(pcap_filename)
+
+packet_array.each do |pkt|
+  packet = PacketFu::Packet.parse(pkt)
+	
+  # Do some stuff here (really any thing you want)
+  puts packet.class  
+end
